4bd62bdcf5b9b1b7b87be05afcd88203b6d7d968
Airscreem <polar5k@mail.ru>
2016-07-13 12:59:41 +0300

    1.2

--- popup.js
+++ popup.js
@@ -1,20 +1,22 @@
 (function() {
+  //console.log(Rx);
+
   var data = {
     listName: document.querySelectorAll('.list-name'),
 
     tabsOther: document.getElementsByClassName('b-themes-tabs-other')[0],
     tabsGeneral: document.getElementsByClassName('b-themes-tabs-general')[0],
+    leftBlock: document.getElementsByClassName('b-themes-left')[0],
+    insideBlock: document.getElementsByClassName('b-themes-inside')[0],
 
     widthTabsGeneral: document.getElementsByClassName('b-themes-tabs-general')[0].offsetWidth,
     widthTabsOther: document.getElementsByClassName('b-themes-tabs-other')[0].offsetWidth,
-    
+
     arrowRight: document.getElementsByClassName('right-button')[0],
     arrowLeft:document.getElementsByClassName('left-button')[0]
   }
 
-  data.tabsOther.style.left = data.widthGeneral + 'px';
-
-  resizeBlock();
+  data.tabsOther.style.left = data.widthTabsGeneral + 'px';
 
   addEventAndTabs(data.listName, 'click', 'active');
 
@@ -35,29 +37,58 @@
   
   function resizeBlock() {
     var self = this,
-        leftBlock = document.getElementsByClassName('b-themes-left')[0];
         mainWidth = document.getElementsByClassName('b-themes-catalog')[0].offsetWidth;
         rightWidth = document.getElementsByClassName('b-themes-right')[0].offsetWidth;
 
     self.leftWidth = mainWidth - rightWidth;
-    leftBlock.style.width = self.leftWidth + 'px';
+    self.widthTogether = data.widthTabsGeneral + data.widthTabsOther;
+    self.eq = self.widthTogether - self.leftWidth - 14;
+
+    (self.eq >= 0) ? self.checkRight = true : self.checkRight = false
+
+    data.insideBlock.style.width = self.leftWidth + 'px';
   }
 
-  function Carousel() {
+  function Carousel(opt) {
     var self = this,
         arrowRight = data.arrowRight,
         arrowLeft = data.arrowLeft;
 
-    self.widthTogether = data.widthTabsGeneral + data.widthTabsOther;
-
+    self.init = init;
     self.visible = visible;
     self.hidden = hidden;
+    self.scroll = 100;
+    self.count = 0;
 
-    resizeBlock.apply(self, arguments);
+    arrowRight.addEventListener('click', clickRight);
+    arrowLeft.addEventListener('click', clickLeft);
 
-    (self.widthTogether > self.leftWidth) ? self.checkRight = true : self.checkRight = false
+    function init() {
+      carousel.resize();
 
-    self.checkRight ? visible(arrowRight) : hidden(arrowRight)
+      if(self.checkRight) {
+        visible(arrowRight);
+      } else {
+        hidden(arrowRight);
+        hidden(arrowLeft);
+      }
+    }
+
+    function clickRight() {
+      self.count += self.scroll;
+      data.insideBlock.scrollLeft += self.scroll;
+      (data.insideBlock.scrollLeft != 0) ? visible(arrowLeft) : hidden(arrowLeft)
+      if(self.count > self.eq) {
+        hidden(arrowRight);
+      }
+    }
+
+    function clickLeft() {
+      self.count -= self.scroll;
+      data.insideBlock.scrollLeft -= self.scroll;
+      (data.insideBlock.scrollLeft != 0) ? visible(arrowLeft) : hidden(arrowLeft)
+      visible(arrowRight);
+    }
 
     function visible(tag) {
       tag.style.display = 'block';
@@ -68,10 +99,17 @@
     }
   }
 
+  Carousel.prototype.resize = function() {
+    resizeBlock.apply(this, arguments);
+  }
+
   var carousel = new Carousel();
+  carousel.init();
   console.log(carousel);
 
   window.addEventListener('resize', function() {
-    resizeBlock();
-  });
+    carousel.init();
+    carousel.count = 0;
+    console.log(this);
+  }.bind(carousel));
 })();
